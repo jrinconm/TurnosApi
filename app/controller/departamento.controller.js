a7f4309656da8425da387da83ed5e02267fda195
Julian <julianrinconm@gmail.com>
2021-04-08 21:40:32 +0200

    Creada parte R de Crud

--- app/controller/departamento.controller.js
+++ app/controller/departamento.controller.js
@@ -1,7 +1,7 @@
 const db = require("../models");
 const Departamento = db.Departamento;
 // Operadores de Sequelize para condiciones
-//const Op = db.Sequelize.Op;
+const Op = db.Sequelize.Op;
 
 // Crear departamento
 exports.create = (req, res) => {
@@ -29,17 +29,57 @@ exports.create = (req, res) => {
       });
     });
 };
-/*
-// Creo el skell basico
+
 // Obtener todos
-exports.findAll = (req, res) => {};
+exports.findAll = (req, res) => {
+  Departamento.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error al obtener los departamentos.",
+      });
+    });
+};
 
 // Buscar por id
-exports.findOne = (req, res) => {};
+exports.findOne = (req, res) => {
+  const id_departamento = req.params.id;
+
+  Departamento.findByPk(id_departamento)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          "Error retrieving Departamento with id=" +
+          id_departamento +
+          " " +
+          err,
+      });
+    });
+};
 
 // Busqueda por nombre
-exports.findByName = (req, res) => {};
+exports.findByName = (req, res) => {
+  const departamento = req.query.departamento;
 
+  Departamento.findAll({
+    where: { departamento: { [Op.like]: `%${departamento}%` } },
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error al obtener los departamentos.",
+      });
+    });
+};
+/*
+// Creo el skell basico
 // Actualizar por id
 exports.update = (req, res) => {};
 
