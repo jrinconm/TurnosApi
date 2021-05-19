b2a964541f66fb1014be3ef5e57a42c02cf3f481
Julian Rincon <julianrinconm@gmail.com>
2021-05-19 15:59:42 +0200

    Creación de contenedor

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Descomentar si queremos resetear la BBDD
-/*
+
 const db = require("./app/models");
 var mysql = require("mysql");
 // De momento para poner la contraseña a fuego
@@ -33,7 +33,7 @@ const EstadoDia = db.EstadoDia;
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and Resync Db");
   poblar();
-}); 
+});
 
 // Pueblo con datos de prueba
 function poblar() {
@@ -134,7 +134,7 @@ consulta.forEach((element) =>
 );
 
 conexion.end();
-*/
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Sin acceso al raiz" });
