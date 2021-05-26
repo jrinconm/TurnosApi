caa3a5b0b3a8e3a9125ac3288355bc3abf8ad518
Julian <julianrinconm@gmail.com>
2021-05-26 09:36:56 +0200

    Eliminar retornos innnecesarios

--- app/models/index.js
+++ app/models/index.js
@@ -1,10 +1,8 @@
 const config = require("../config/db.config.js");
-
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
   host: config.HOST,
   dialect: config.dialect,
-
   pool: {
     max: config.pool.max,
     min: config.pool.min,
